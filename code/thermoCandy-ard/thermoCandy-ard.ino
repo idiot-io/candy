8b768e3040e690468e1c931d4be7ff0cb52cf9ea
5shekel <yair99@gmail.com>
2020-07-25 23:26:43 +0300

    nicer blink up

--- code/thermoCandy-ard/thermoCandy-ard.ino
+++ code/thermoCandy-ard/thermoCandy-ard.ino
@@ -34,6 +34,17 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, pixelPin, NEO_GRB + NEO_KHZ800);
 Average<float> ave(30);
 int sensorPin = A2; //pin 3
 
+
+//pixels
+byte red, green, blue;
+
+//#define range 0 //RAINBOWS
+#define range 1 //test heat
+//#define range 2   //fancy
+
+int ledState, getout, blinkRate;
+unsigned long prevMillis1, prevMillis2  = 0;
+
 void setup() {
   mySerial.begin(9600);
 
@@ -41,6 +52,7 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
 
   int analogValue = analogRead(sensorPin);
   long temperature = (readVcc() / 1000.0) * analogValue * 100.0 / 1024;
@@ -49,49 +61,100 @@ void loop() {
   float average = ave.mean();
 
 
-  byte red, green, blue;
-  if (average > 20  && average < 35) {
-    green = mapfloat(average, 20, 37, 0, 255);
-    blue = 255 - red;
-    red = 0;
-  } else if (average >= 35  && average < 36.5) {
-    red = mapfloat(average, 24, 37, 0, 255);
-    green = 255 - red;
-    blue = 0;
-  } else if (average >= 36.5) {
-    red = mapfloat(average, 36.5, 37, 0, 255);
-    green = 0;
-    blue = 0;
+  switch (range) {
+    case 0:
+      //rainbows pixel test
+      for (int i = 0; i < 255; i++) {
+        green = i;
+        red = i - 255;
+        blue = 0;
+      }
+      break;
+    case 1:
+      getout = 0;
+      if (average > 20  && average < 35) {
+        red = 0;
+        green = 255;
+        blue = 0;
+      } else if (average >= 36.5 && average < 120) {
+        red = 200;
+        green = 0;
+        blue = 0;
+      } else if (average >= 120) {
+
+        getout = 1;
+
+        blinkRate = map( constrain(average, 146, 120) , 146, 120, 500, 50);
+        red = ledState * 255;
+        green = 0;
+        blue = 0;
+      }
+
+
+      break;
+    case 2:
+      //fancy
+      byte red, green, blue;
+      if (average > 20  && average < 35) {
+        green = mapfloat(average, 20, 37, 0, 255);
+        blue = 255 - red;
+        red = 0;
+      } else if (average >= 35  && average < 36.5) {
+        red = mapfloat(average, 24, 37, 0, 255);
+        green = 255 - red;
+        blue = 0;
+      } else if (average >= 36.5 && average < 100) {
+        red = mapfloat(average, 36.5, 37, 0, 255);
+        green = 0;
+        blue = 0;
+      } else if (average >= 100) {
+        red = ledState;
+        green = 0;
+        blue = 0;
+      }
+
+      break;
   }
+
 
   pixels.setPixelColor(0, pixels.Color(red, green, blue));
   pixels.show();
 
-  // And show some interesting results.
-  mySerial.print(ave.mean());
-  mySerial.print(" ");
-  mySerial.print(temperature);
+  if (currentMillis - prevMillis1 >= 50) {
 
-  mySerial.print(" ");
-  mySerial.print(red);
-  mySerial.print(" ");
-  mySerial.print(green);
-  mySerial.print(" ");
-  mySerial.println(blue);
-  /*
-    for (int i = 0; i < 255; i++) {
-    pixels.setPixelColor(0, pixels.Color(i, 255 - i, 0));
-    pixels.show();
-    delay(10);
+    // And show some interesting results.
+    mySerial.print(average);
+    mySerial.print(" ");
+    mySerial.print(temperature);
+
+    mySerial.print(" ");
+    mySerial.print(blinkRate);
+    mySerial.print(" ");
+    mySerial.print(ledState);
+
+    /*
+        mySerial.print(" ");
+        mySerial.print(red);
+        mySerial.print(" ");
+        mySerial.print(green);
+        mySerial.print(" ");
+        mySerial.print(blue);
+    */
+    mySerial.println();
+  }
+
+  //700=interval
+  if (currentMillis - prevMillis2 >= blinkRate) {
+    prevMillis2 = currentMillis;
+    if (ledState) {
+      ledState = false;
+    } else {
+      ledState = true;
     }
-  */
-
-  delay(10);
+  }
 
 
 }
-
-
 // This code snippet is made by Tinkerit. Open Source license. Slightly modified by DavidJWatts
 long readVcc() {
   // Read 1.1V reference against AVcc
@@ -112,4 +175,3 @@ float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
 {
   return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
 }
-
