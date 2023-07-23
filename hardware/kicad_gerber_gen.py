280d0b315d79798703afab8af694d3601dc636fe
devdesk <tami@tami.org.il>
2023-07-23 17:45:55 +0300

    kicad 6 and py 3

--- hardware/kicad_gerber_gen.py
+++ hardware/kicad_gerber_gen.py
@@ -7,76 +7,97 @@
     additons via https://github.com/beagleboard/pocketbeagle/blob/master/kicad-scripts/kicad-fab.py
 '''
 
-import sys, os, subprocess
-
+import sys, os, subprocess, shutil, platform
 from pcbnew import *
+
+if len(sys.argv) < 3:
+    print("Usage: python3 script.py [filename] [gerbv_path]")
+    sys.exit(1)
+# A helper function to convert a UTF8 string for python2 or python3
+def fromUTF8Text( afilename ):
+    if sys.version_info < (3, 0):
+        return afilename.encode()
+    else:
+        return afilename
+
+
+# Rename the .drl file, so it apears first in the list of gerber files
+def get_filename_without_extension(file_path):
+    """
+    This function returns the filename without extension, regardless of the path.
+
+    :param file_path: The complete file path.
+    :return: The filename without extension.
+    """
+    base_name = os.path.basename(file_path)  # get the filename
+    file_name_without_extension = os.path.splitext(base_name)[0]  # remove the extension
+    return file_name_without_extension
+ 
 filename=sys.argv[1]
 gerbv_path=sys.argv[2]
 
-    
-board = LoadBoard(filename)
+gerbv_executable = "gerbv.exe" if platform.system() == "Windows" else "gerbv"
+if shutil.which(gerbv_executable) is None:
+    print(f"{gerbv_executable} is not installed or not found on system PATH.")
+    sys.exit(1)
 
+board = LoadBoard(filename)
 plotDir = os.path.dirname(filename)+"/plot/"
 
+
 pctl = PLOT_CONTROLLER(board)
-
 popt = pctl.GetPlotOptions()
-
 popt.SetOutputDirectory(plotDir)
 
 # Set some important plot options:
-popt.SetPlotFrameRef(False)     #do not change it
-popt.SetLineWidth(FromMM(0.15)) # default line width to plot items having no line thickiness defined
+# One cannot plot the frame references, because the board does not know
+# the frame references.
+popt.SetPlotFrameRef(False)
+popt.SetSketchPadLineWidth(FromMM(0.1))
 
-popt.SetAutoScale(False)        #do not change it
-popt.SetScale(1)                #do not change it
-popt.SetMirror(False)
-popt.SetUseGerberAttributes(False) #true will set it to gerber x2, manyboard fab houses dont like this
-popt.SetUseGerberProtelExtensions(True) # change extension from default .gbr
-popt.SetExcludeEdgeLayer(True); # true will include edge in copper, not good
+popt.SetAutoScale(False)
 popt.SetScale(1)
+popt.SetMirror(False)
+popt.SetUseGerberAttributes(False) #gerbv doesnt like gerber x2 attributes
+# popt.SetIncludeGerberNetlistInfo(True)
+# popt.SetUseGerberProtelExtensions(False)
 popt.SetUseAuxOrigin(True)
 
-# This by gerbers only
-popt.SetSubtractMaskFromSilk(False)
-# Disable plot pad holes
-popt.SetDrillMarksType( PCB_PLOT_PARAMS.NO_DRILL_SHAPE );
-# Skip plot pad NPTH when possible: when drill size and shape == pad size and shape
-# usually sel to True for copper layers
-popt.SetSkipPlotNPTH_Pads( False );
+# This by gerbers only (also the name is truly horrid!)
+popt.SetSubtractMaskFromSilk(False) #remove solder mask from silk to be sure there is no silk on pads
 
-# param 0 is the layer ID
-# param 1 is a string added to the file base name to identify the drawing
-# param 2 is a comment
-# Create filenames in a way that if they are sorted alphabetically, they
-# are shown in exactly the layering the board would look like. So
-#   gerbv *
-# just makes sense. The drill-file will be numbered 00 so that it is first.
+#Create a pdf file of the top silk layer
+# pctl.SetLayer(F_SilkS)
+# pctl.OpenPlotfile("Silk", PLOT_FORMAT_PDF, "Assembly guide")
+# pctl.PlotLayer()
+
+# Once the defaults are set it become pretty easy...
+# I have a Turing-complete programming language here: I'll use it...
+# param 0 is a string added to the file base name to identify the drawing
+# param 1 is the layer ID
 plot_plan = [
-    ( Edge_Cuts, "01-Edge_Cuts",   "Edges" ),
-
-    ( F_SilkS,   "02-SilkTop",     "Silk top" ),
-    ( F_Mask,    "03-MaskTop",     "Mask top" ),
-    ( F_Cu,      "04-CuTop",       "Top layer" ),
-
-    ( B_SilkS,   "05-SilkBottom",  "Silk top" ),
-    ( B_Mask,    "06-MaskBottom",  "Mask bottom" ),
-    ( B_Cu,      "07-CuBottom",    "Bottom layer" ),
+    ("01-Edge_Cuts", Edge_Cuts, "Edges"),
+    ("02-SilkTop", F_SilkS, "Silk top"),
+    ("03-MaskTop", F_Mask, "Mask top"),
+    ("04-CuTop", F_Cu, "Top layer"),
+    ("05-SilkBottom", B_SilkS, "Silk top"),
+    ("06-MaskBottom", B_Mask, "Mask bottom"),
+    ("07-CuBottom", B_Cu, "Bottom layer"),
 ]
 
-
-
+# In Gerber format, Set layer before calling OpenPlotfile is mandatory to generate
+# the right Gerber file header.
 for layer_info in plot_plan:
     if layer_info[1] <= B_Cu:
         popt.SetSkipPlotNPTH_Pads( True )
     else:
         popt.SetSkipPlotNPTH_Pads( False )
 
-    pctl.SetLayer(layer_info[0])
-    pctl.OpenPlotfile(layer_info[1], PLOT_FORMAT_GERBER, layer_info[2])
-    print 'plot %s' % pctl.GetPlotFileName()
-    if pctl.PlotLayer() == False:
-        print "plot error"
+    pctl.SetLayer(layer_info[1])
+    pctl.OpenPlotfile(layer_info[0], PLOT_FORMAT_GERBER, layer_info[2])
+
+    print( 'plot %s' % fromUTF8Text( pctl.GetPlotFileName() ) )
+    pctl.PlotLayer()
 
 # At the end you have to close the last plot, otherwise you don't know when
 # the object will be recycled!
@@ -89,7 +110,7 @@ drlwriter.SetMapFileFormat( PLOT_FORMAT_PDF )
 
 mirror = False
 minimalHeader = False
-offset = wxPoint(0,0)
+offset = VECTOR2I(0,0)
 # False to generate 2 separate drill files (one for plated holes, one for non plated holes)
 # True to generate only one drill file
 mergeNPTH = True
@@ -100,27 +121,17 @@ drlwriter.SetFormat( metricFmt )
 
 genDrl = True
 genMap = True
-print 'create drill and map files in %s' % pctl.GetPlotDirName()
+print( 'create drill and map files in %s' % fromUTF8Text( pctl.GetPlotDirName() ) )
 drlwriter.CreateDrillandMapFilesSet( pctl.GetPlotDirName(), genDrl, genMap );
 
+# One can create a text file to report drill statistics
+rptfn = pctl.GetPlotDirName() + 'drill_report.rpt'
+drlwriter.GenDrillReportFile( rptfn );
 
-# We can't give just the filename for the name of the drill file at generation
-# time, but we do want its name to be a bit different to show up on top.
-# So this is an ugly hack to rename the drl-file to have a 0 in the beginning.
-base_name = os.path.splitext(os.path.basename(filename))[0]
-print base_name
 
-drill_file = os.path.dirname(filename) + "/plot/" + base_name
-print 'drill file: '+ drill_file
-
-if os.path.exists(drill_file+ "-00.drl"):
-    print 'file exists, skip rename'
+drill_file = pctl.GetPlotDirName() + get_filename_without_extension(filename)
+print("rename " + drill_file + ".drl to " + drill_file + "-00.drl")
+if os.path.exists(drill_file + "-00.drl"):
+    print('file exists, skip rename')
 else:
-    os.rename(drill_file+ ".drl", drill_file+ "-00.drl")
-
-if os.name == 'nt':
-    os.system(gerbv_path+'/gerbv.exe '+ os.path.dirname(filename)+'/plot/*')
-    pass # Windows
-else:
-    os.system('gerbv plot/*')
-    pass # other (unix)
+    os.rename(drill_file + ".drl", drill_file + "-00-drill.drl")
